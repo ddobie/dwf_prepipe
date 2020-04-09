711f1230b8aba899b07ff2cd1bf3b61694d104b3
Frank Stars <webb.sara.a@gmail.com>
2020-04-09 16:15:13 +1000

    commit of exisiting prepipe scripts on fstars

---
+++ dwf_prepipe_preprocess_new.py
@@ -0,0 +1,230 @@
+import os
+import numpy as np
+from numpy import ma
+
+__whatami__ = 'Bias-correct, flat-field, astrometically calibrate, '\
+              'and mask DECam images.'
+__author__ = 'Danny Goldstein <danny@caltech.edu>'
+
+def _read_clargs(val):
+    if val[0].startswith('@'):
+        # then its a list
+        val = np.genfromtxt(val[0][1:], dtype=None, encoding='ascii')
+        val = np.atleast_1d(val)
+    return np.asarray(val)
+
+def _parse_doubleslice(header, key, overscan=False):
+    # get the good sections of the images
+    goodsec = [tuple(map(int, s.split(':'))) for s in header[key][1:-1].split(',')]
+    slices = [slice(c[0] - 1, c[1]) for c in goodsec]
+    if overscan:
+        slices[0] = slice(goodsec[0][0] - 1 + 5, goodsec[0][1] - 5)
+    return slices[::-1]
+
+def overscan_and_mask_single(hdu):
+    # Correct an image for overscan and create bad pixel masks based on
+    # saturated pixels
+
+    # create some data structures to store the output masks
+
+    hdu.data = hdu.data.astype('float32')
+
+    for amp in ['A', 'B']:
+        over1, over2 = _parse_doubleslice(hdu.header, f'BIASSEC{amp}', overscan=True)
+        data1, data2 = _parse_doubleslice(hdu.header, f'DATASEC{amp}')
+        overbias = hdu.data[over1, over2].mean(axis=1)
+        hdu.data[data1, data2] -= overbias[:, None]
+
+    # do the saturation correction
+    image = hdu.data
+    mask = np.zeros_like(image, dtype='uint8')
+
+    for amp in ['A', 'B']:
+         saturval = hdu.header[f'SATURAT{amp}']
+         amp1, amp2 = _parse_doubleslice(hdu.header, f'CCDSEC{amp}')
+         mask[amp1, amp2][image[amp1, amp2] >= saturval] += 2 ** 0
+
+    # create an HDUList object for the masks
+    maskhdu = fits.ImageHDU(data=mask, header=hdu.header)
+
+    return hdu, maskhdu
+
+# recursively partition an iterable into subgroups
+_split = lambda iterable, n: [iterable[:len(iterable)//n]] + \
+    _split(iterable[len(iterable)//n:], n - 1) if n != 0 else []
+
+
+        
+if __name__ == '__main__':
+    import pdb
+    import subprocess
+    from argparse import ArgumentParser
+    from astropy.io import fits
+    
+    parser = ArgumentParser()
+    parser.add_argument('--flat-frames', required=True, 
+                        help='Frames to use for flat fielding, in the same '\
+                        'order as --input-frames. If the argument is prepended '\
+                        'with "@", then it will be treated as a list containing '\
+                        'the names of the flats, one per line.',
+                        dest='flats', nargs='+')
+
+    parser.add_argument('--bias-frames', required=True,
+                        help='Frames to use for bias subtraction, in the same '\
+                        'order as --input-frames. If the argument is prepended '\
+                        'with "@", then it will be treated as a list containing '\
+                        'the names of the biases, one per line.',
+                        dest='biases', nargs='+')
+
+    parser.add_argument('--input-frames', required=True,
+                        help='Raw input frames. If the argument is prepended with '\
+                        '"@", then it will be treated as a list containing the names '\
+                        'of the input frames, one per line.',
+                        dest='frames', nargs='+')
+    
+    parser.add_argument('--badcol-mask', required=True,
+                        help='List of badcol mask files.',
+                        dest='colmask', nargs=1)
+
+    parser.add_argument('--with-scamp-exec', required=False, default=None,
+                        help='Use this executable for scamp instead of the system default.',
+                        dest='scampbin', nargs=1)
+
+    parser.add_argument('--man-gaia', required=False, default=False, 
+                        help='manual imput of gaia catalog.', dest='gaia_source')
+
+    parser.add_argument('--use-mpi', required=False, default=False, 
+                        help='Parallelize with MPI.', dest='mpi', action='store_true')
+
+    args = parser.parse_args()
+
+    if args.mpi:
+        from mpi4py import MPI
+
+        # set up mpi
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        size = comm.Get_size()
+
+    
+    # read the argparse input
+    flats = _read_clargs(args.flats)
+    biases = _read_clargs(args.biases)
+    frames = _read_clargs(args.frames)
+    masks = _read_clargs(['@' + args.colmask[0]])
+    gaia_source = _read_clargs(args.gaia_source)
+
+    # distribute the work to the worker processes
+
+    if args.mpi:
+        ind = np.arange(len(flats))
+        myind = _split(ind, size)[rank]
+    
+        myflats = flats[myind]
+        mybiases = biases[myind]
+        myframes = frames[myind]
+        
+    else:
+        myflats = flats
+        mybiases = biases
+        myframes = frames
+    
+    # list a few astromatic config files
+    wd = os.path.dirname(__file__)
+    confdir = os.path.join(wd, 'config')
+    sexconf = os.path.join(confdir, 'scamp.sex')
+    nnwname = os.path.join(confdir, 'default.nnw')
+    filtname = os.path.join(confdir, 'default.conv')
+    paramname = os.path.join(confdir, 'scamp.param')    
+    scampconf = os.path.join(confdir, 'scamp.conf')
+    missfitsconf = os.path.join(confdir, 'missfits.conf')
+    
+    # pass these constant options to sextractor
+    clargs = ' -PARAMETERS_NAME %s -FILTER_NAME %s -STARNNW_NAME %s' % (paramname, filtname, nnwname)
+
+    for flat, bias, frame in zip(myflats, mybiases, myframes):
+        #Overscan
+        with fits.open(frame) as hdul:
+            ihdu, mhdu = overscan_and_mask_single(hdul[0])
+            ccdnum_header = hdul[0].header["CCDNUM"]
+            bpm_name = f"/home/fstars/dwf_prepipe/bpm/DECam_Master_20140209v2_cd_{format(ccdnum_header, '02d')}.fits"
+        with fits.open(flat) as fl, fits.open(bias) as b, fits.open(bpm_name) as bp:
+
+            fhdu = fl[0]
+            bhdu = b[0]
+            bphdu = bp[0]
+            TRIM1, TRIM2 = _parse_doubleslice(ihdu.header, 'DATASEC')
+
+            #Overscan the bias
+            bhdu, mbhdu = overscan_and_mask_single(bhdu)
+            # do the bias correction
+            ccdnum = '%02d' % ihdu.header['CCDNUM']
+
+            with np.errstate(divide='ignore', invalid='ignore'):
+                ihdu.data[TRIM1, TRIM2] -= bhdu.data[TRIM1, TRIM2]
+
+            # do the flat fielding
+            fl1, fl2 = _parse_doubleslice(fhdu.header, 'DATASEC')
+            flfield = fhdu.data[fl1, fl2]
+
+            with np.errstate(divide='ignore', invalid='ignore'):
+                calibpix = ihdu.data[TRIM1, TRIM2] / (flfield / np.median(flfield))
+
+            # mask any resulting pixels that are invalid from the previous operation
+            mhdu.data[TRIM1, TRIM2][~np.isfinite(calibpix)] += 2 ** 1
+
+            # and mask any that are bad from the badcol mask
+            mhdu.data[TRIM1, TRIM2][bphdu.data != 0] += 2 ** 2
+
+            # get the final mask
+            finalmask = mhdu.data[TRIM1, TRIM2]
+
+            # change the values of the science image to 1e-30 where there is a bad pixel
+            index_bad = np.where(finalmask != 0)
+            calibpix[index_bad] = np.nan
+
+            # save the flat-fielded and bias-corrected image to a new fits image
+            sciname = frame
+            mskname = frame.replace('.fits', f'.mask.fits').replace('.fz', '')
+
+            # update the header
+            delkwds = []
+            for card in ihdu.header.cards:
+                if 'SEC' in card.keyword and card.keyword != 'DETSEC':
+                    delkwds.append(card.keyword)
+
+            for kwd in delkwds:
+                del ihdu.header[kwd]
+
+            hdul = fits.PrimaryHDU(calibpix.astype('float32'), header=ihdu.header)
+            hdul.writeto(sciname, overwrite=True)
+            mskhdul = fits.PrimaryHDU(finalmask, header=ihdu.header)
+            mskhdul.writeto(mskname, overwrite=True)
+            
+            # now prepare to run source extractor
+            syscall = 'sex -c %s -CATALOG_NAME %s -CHECKIMAGE_NAME %s %s'
+            catname = frame.replace('fits', 'cat')
+            chkname = frame.replace('fits', 'noise.fits')
+            syscall = syscall % (sexconf, catname, chkname, sciname)
+            syscall += clargs
+            print(syscall)
+
+            # call it
+            subprocess.check_call(syscall.split())
+            print(f'sextractor complete for {sciname}')
+            
+            # now run scamp
+            syscall = 'scamp -c %s %s -ASTREF_CATALOG FILE -ASTREFCAT_NAME %s -ASTREFCENT_KEYS RA_ICRS,DE_ICRS -ASTREFERR_KEYS e_RA_ICRS,e_DE_ICRS -ASTREFMAG_KEY Gmag' % (scampconf, catname, gaia_source)
+            if args.scampbin is not None:
+                syscall = args.scampbin[0] + syscall[5:]
+            print(syscall)
+            subprocess.check_call(syscall.split())
+
+            # fix the header
+            subprocess.check_call(["/home/fstars/missfits-2.8.0/bin/missfits",frame,frame.replace(".fits",".head"),f"-c {missfitsconf}"])
+
+            print(f'scamp complete for {sciname}')
+            
+            
+            
+            
---
+++ dwf_prepipe_processccd2.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python3
+import os
+import datetime
+import time
+import shutil
+import argparse
+import subprocess
+import glob
+
+#~/.astropy/config/astropy.cfg was getting messed up - seperate default (used by pipeloop?) and this
+# os.environ['XDG_CONFIG_HOME']='/home/fstars/.python3_config/'
+
+import astropy.io.fits as pyfits
+import astropy.units as u 
+
+def check_path(path):
+    """Check if path ends with a slash ('/'). Else, it adds a slash.
+
+    The function also creates the directory if it does not existing.
+
+    Parameters
+    ----------
+    path : str
+        A path
+
+    Returns
+    -------
+    path : str
+        A functional path
+    """
+    from os import makedirs
+    from os.path import exists
+
+    if len(path) > 0 and path[-1] != '/':
+        path = path + '/'
+
+    if not exists(path):
+        makedirs(path)
+
+    return path
+
+
+def check_wcs(ut,ccd,expnum):
+    pipeloop_out=subprocess.check_output(['pipeview.pl','-red',ut,ccd,'-stage','WCSNON','-id',str(expnum)],stderr=subprocess.STDOUT,universal_newlines=True)
+    wcs_val=pipeloop_out.splitlines()[9].strip(' \t\n\r').split(' ')[-1]
+    return (wcs_val == 'X')
+
+def get_shift_ccd(ut,ccd,Field,expnum):
+    pipeview_out=subprocess.check_output(['pipeview.pl','-red',ut,ccd,'-stage','WCSNON','-wcs','-im',Field],stderr=subprocess.STDOUT,universal_newlines=True)
+    pipeview_out=pipeview_out.splitlines()[2:]
+
+    rashift=[]
+    decshift=[]
+
+    ra_close=[]
+    dec_close=[]
+
+    for line in pipeview_out:
+        if((line.split()[0].split('.')[0] == Field) and (line.split()[1] == '1')):    #Checks Valid Line
+            rashift.append(float(line.strip(' \t\n\r').split()[5]))
+            decshift.append(float(line.strip(' \t\n\r').split()[6]))
+            line_exp=line.split()[0].split('_')[0].split('.')[-1]
+            if(abs(int(line_exp) - int(expnum)) < 7):
+                ra_close.append(float(line.strip(' \t\n\r').split()[5]))
+                dec_close.append(float(line.strip(' \t\n\r').split()[6]))
+
+    if((len(rashift) == 0) or (len(decshift) == 0)):
+        return [0,0]
+    if((len(ra_close) != 0) or (len(dec_close) != 0)):
+        return[sum(ra_close)/len(ra_close),sum(dec_close)/len(dec_close)]
+    else:
+        return[sum(rashift)/len(rashift),sum(decshift)/len(decshift)]
+
+def get_shift_exp(ut,ccd,exp,Field):
+    pipeview_out=subprocess.check_output(['pipeview.pl','-red',ut,'1-60','-stage','WCSNON','-wcs','-id',exp],stderr=subprocess.STDOUT,universal_newlines=True)
+    pipeview_out=pipeview_out.splitlines()[1:]
+
+    rashift=[]
+    decshift=[]
+
+    ra_close=[]
+    dec_close=[]
+
+    for line in pipeview_out:
+        if((line.split()[0].split('.')[0] == Field) and (line.split()[1] == '1')): #Checks Valid Line
+            rashift.append(float(line.strip(' \t\n\r').split()[5]))
+            decshift.append(float(line.strip(' \t\n\r').split()[6]))
+
+            line_ccd=line.split()[0].split('_')[1]
+            if(abs(int(line_ccd) - int(ccd)) < 3):
+                ra_close.append(float(line.strip(' \t\n\r').split()[5]))
+                dec_close.append(float(line.strip(' \t\n\r').split()[6]))
+
+    if((len(rashift) == 0) or (len(decshift) == 0)):
+        return [0,0]
+    if((len(ra_close) != 0) or (len(dec_close) != 0)):
+        return[sum(ra_close)/len(ra_close),sum(dec_close)/len(dec_close)]
+    else:
+        return[sum(rashift)/len(rashift),sum(decshift)/len(decshift)]
+
+
+def get_shift_field(ut,ccd,exp,Field):
+    pipeview_out=subprocess.check_output(['pipeview.pl','-red',ut,'1-60','-stage','WCSNON','-wcs','-im',Field],stderr=subprocess.STDOUT,universal_newlines=True)
+    pipeview_out=pipeview_out.splitlines()[2:]
+
+    rashift=[]
+    decshift=[]
+
+    ra_close=[]
+    dec_close=[]
+
+    for line in pipeview_out:
+        if((line.split()[0].split('.')[0] == Field) and (line.split()[1] == '1')): #Checks Valid Line
+            rashift.append(float(line.strip(' \t\n\r').split()[5]))
+            decshift.append(float(line.strip(' \t\n\r').split()[6]))
+
+            line_ccd=line.split()[0].split('_')[1]
+            if(abs(int(line_ccd) - int(ccd)) < 3):
+                ra_close.append(float(line.strip(' \t\n\r').split()[5]))
+                dec_close.append(float(line.strip(' \t\n\r').split()[6]))
+
+    if((len(rashift) == 0) or (len(decshift) == 0)):
+        return [0,0]
+    if( (len(ra_close) != 0) or (len(dec_close) != 0) ):
+        return[sum(ra_close)/len(ra_close),sum(dec_close)/len(dec_close)]
+    else:
+        return[sum(rashift)/len(rashift),sum(decshift)/len(decshift)]
+
+def main():
+    DWF_Push = '/fred/oz100/pipes/DWF_PIPE/CTIO_PUSH/'
+    use_local=1
+
+    parser = argparse.ArgumentParser(description='DWF_Prepipe process for single compressed CCD', formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-p','--push_dir',metavar='DIRECTORY',type=str,default=DWF_Push,
+        help='Directory where taballs of compressed files are placed')
+    parser.add_argument('-l','--local',metavar='DIRECTORY',type=int,default=use_local,
+        help='Use node local storage for jpg to fits conversion? (1 or 0)')
+    parser.add_argument('-i','--input_file',type=str,help='input .jp2 file')
+
+    args = parser.parse_args()
+
+    #Set local Directory and check to see if it exists
+    # local_dir=os.path.join(os.environ['JOBFS'],'dwf')
+    local_dir = '/fred/oz100/pipes/DWF_PIPE/CTIO_PUSH/untar/'
+    local_convert=args.local
+
+    if(local_convert):
+        if not os.path.isdir(local_dir):
+            print('Creating Directory: '+local_dir)
+            os.makedirs(local_dir)
+
+
+
+    photepipe_rawdir= check_path('/fred/oz100/pipes/arest/DECAM/DEFAULT/rawdata/')
+    push_dir=args.push_dir
+    untar_path=check_path(push_dir+'untar/')
+
+    file_name=args.input_file
+    DECam_Root=file_name.split('.')[0]
+    ccd_num=DECam_Root.split('_')[2]
+
+    if(local_convert):
+        #Move .jp2 to local directory
+        print('Moving '+untar_path+file_name+' to '+local_dir+file_name)
+        shutil.move(untar_path+file_name,local_dir+file_name)
+        untar_path=local_dir
+
+    #Uncompress Fits on local Directory
+    uncompressed_fits=untar_path+DECam_Root+'.fits'
+    print('--------*****')
+    print(uncompressed_fits)
+    print('Uncompressing: '+file_name+' in path: '+untar_path)
+    print('--------*****')
+    print(['j2f_DECam','-i',untar_path+file_name,'-o',uncompressed_fits,'-num_threads',str(1)])
+    subprocess.run(['j2f_DECam','-i',untar_path+file_name,'-o',uncompressed_fits,'-num_threads',str(1)])
+
+    #Extract nescessary information from file for naming scheme
+    exp=pyfits.getval(uncompressed_fits,"EXPNUM")
+    Field=pyfits.getval(uncompressed_fits,"OBJECT")
+    Filter=pyfits.getval(uncompressed_fits,"FILTER")[0]
+
+    #FOR Chile!
+    ##FIX THIS.  So the problem is in a night's observations can straddle two different ut's
+    ##The initial fits works but doesn't straddle month's, plus since the check is based off of
+    ## CURRENT time NOT observed time it can screw up on reprocessing data.  Fix both of these.
+    timestamp=datetime.datetime.utcnow().time()
+    ut = 'ut191207'
+    #if(timestamp > datetime.time(22,30)):
+        #ut='ut'+str(int(pyfits.getval(uncompressed_fits,"OBSID")[6:12])+1)
+    #else:
+        #ut='ut'+pyfits.getval(uncompressed_fits,"OBSID")[6:12]
+
+    obstype=pyfits.getval(uncompressed_fits,"OBSTYPE")
+
+    newname=Field+'.'+Filter+'.'+ut+'.'+str(exp)+'_'+ccd_num+'.fits'
+    if((obstype == 'dome flat') or (obstype == 'domeflat')):
+        newname='domeflat.'+Filter+'.'+ut+'.'+str(exp)+'_'+ccd_num+'.fits'
+    if((obstype == 'zero') or (obstype == 'bias')):
+        newname='bias.'+ut+'.'+str(exp)+'_'+ccd_num+'.fits'
+
+    ut_dir=check_path(photepipe_rawdir+ut+'/')
+    dest_dir=check_path(ut_dir+ccd_num+'/')
+
+
+    #Check to see if UT date & CCD Directories have been created
+    if not os.path.isdir(ut_dir):
+        print('Creating Directory: '+ut_dir)
+        os.makedirs(ut_dir)
+    else:
+        print('Directory Exists: '+ut_dir)
+
+    if not os.path.isdir(dest_dir):
+        print('Creating Directory: '+dest_dir)
+        os.makedirs(dest_dir)
+    else:
+        print('Directory Exists: '+dest_dir)
+
+    #Same as above but for the $workspace
+    if not os.path.isdir(ut_dir.replace("rawdata","workspace")):
+        print('Creating Directory: '+ut_dir.replace("rawdata","workspace"))
+        os.makedirs(ut_dir.replace("rawdata","workspace"))
+    else:
+        print('Directory Exists: '+ut_dir.replace("rawdata","workspace"))
+
+    if not os.path.isdir(dest_dir.replace("rawdata","workspace")):
+        print('Creating Directory: '+dest_dir.replace("rawdata","workspace"))
+        os.makedirs(dest_dir.replace("rawdata","workspace"))
+    else:
+        print('Directory Exists: '+dest_dir.replace("rawdata","workspace"))
+
+
+    #Move Uncompressed Fits File
+    print('Renaming '+file_name+' to '+newname)
+    print('And moving to: '+dest_dir)
+    shutil.move(uncompressed_fits,dest_dir+newname)
+
+
+    #Check for and prepare the calibration file lists
+    checkflats=glob.glob(dest_dir+"domeflat."+Filter+".master.*" )
+
+    if checkflats==[]:
+        print("Prepipe Warning: No master flat detected! Looking for an individual flat..")
+        checkflats=glob.glob(dest_dir+"domeflat."+Filter+"*" )
+        if checkflats == []:
+            print("Prepipe Error: No flats detected! Exiting ")
+            exit()
+    #Use the first in the list
+    flat=checkflats[0]
+
+    checkbias = glob.glob(dest_dir+"bias.master.*")
+    if checkbias==[]:
+        print("Prepipe Warning: No master bias detected! Looking for an individual bias..")
+        checkbias=glob.glob(dest_dir+"bias.*" )
+        if checkbias==[]:
+            print("Prepipe Error: No bias detected! Exiting ")
+            exit()
+    #Uuse the first in the list
+    bias=checkbias[0]    
+
+    #Copy the raw image to the workspace, so that all the products 
+    #will be generated there.
+    subprocess.check_call(['cp',dest_dir+newname, dest_dir.replace("rawdata","workspace")+newname])
+
+    #Call Danny's preprocess code for CCD reduction
+    subprocess.check_call(['python','/home/fstars/dwf_prepipe/dwf_prepipe_preprocess_new.py', \
+    "--input-frames="+dest_dir.replace("rawdata","workspace")+newname, \
+    "--flat-frames="+flat, "--bias-frames="+bias, "--badcol-mask=/home/fstars/dwf_prepipe/masks","--with-scamp-exec=/home/fstars/scamp_gaia/bin/scamp", \
+    f"--man-gaia=/fred/oz100/pipes/DWF_PIPE/GAIA_DR2/{Field}_gaia_dr2_LDAC.fits"] )
+
+    #Remove unescessary .jp2
+    print('Deleting: '+untar_path+file_name)
+    subprocess.run(['rm',untar_path+file_name])
+
+if __name__ == '__main__':
+    main()
