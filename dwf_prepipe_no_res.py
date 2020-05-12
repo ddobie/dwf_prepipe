8b58234382f0e36f33efd73cd5cfeeef4087fe3f
Frank Stars <webb.sara.a@gmail.com>
2020-05-12 23:27:25 +1000

    updated dwf_prepipe_no_res and res to take in the ut date from command line

--- dwf_prepipe_no_res.py
+++ dwf_prepipe_no_res.py
@@ -10,15 +10,8 @@ import multiprocessing
 import subprocess
 import astropy.io.fits as pyfits
 
-###############################################################
-###     CHANGE RESIVATION NAME HERE IF NEEDED           ######
-#res_name = 'dwf'
-run_date = 'ut2020testing'
-###############################################################
-
-
 #Uncompress new file + create & submit assosciated sbatch scripts
-def dwf_prepipe_unpack(file_name,push_path,untar_path,sbatch_path):
+def dwf_prepipe_unpack(file_name,push_path,untar_path,sbatch_path,run_date):
 	ccdlist=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59']
 	#Truncated CCD list for testing
 	#ccdlist=['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16'] #truncated version for test
@@ -39,10 +32,10 @@ def dwf_prepipe_unpack(file_name,push_path,untar_path,sbatch_path):
 	n_scripts=math.ceil(len(ccdlist)/n_per_ccd)
 	print('Writing '+str(n_scripts)+' sbatch scripts for '+file_name)
 	for n in range(n_scripts):
-		dwf_prepipe_sbatchccds(DECam_Root,DECam_Root+'_q'+str(n+1),ccdlist[n_per_ccd*n:(n+1)*n_per_ccd],sbatch_path,push_path)
+		dwf_prepipe_sbatchccds(DECam_Root,DECam_Root+'_q'+str(n+1),ccdlist[n_per_ccd*n:(n+1)*n_per_ccd],sbatch_path,push_path, run_date)
 
 #Write Qsub Script & submit to queue
-def dwf_prepipe_sbatchccds(filename_root,qroot,ccds,sbatch_path,push_path):
+def dwf_prepipe_sbatchccds(filename_root,qroot,ccds,sbatch_path,push_path, run_date):
 	image_list=[filename_root+'_'+f+'.jp2' for f in ccds]
 
 	sbatch_out_dir=sbatch_path+'out/'
@@ -58,6 +51,7 @@ def dwf_prepipe_sbatchccds(filename_root,qroot,ccds,sbatch_path,push_path):
 	ppn='16'
 	mem='90G'
 	tmp='4G'
+	
 
 	sbatch_file.write('#!/bin/bash \n')
 	sbatch_file.write('#SBATCH -J {0}\n'.format(qroot))
@@ -107,34 +101,32 @@ def dwf_prepipe_sbatchccds(filename_root,qroot,ccds,sbatch_path,push_path):
 	subprocess.run(['sbatch',sbatch_name])
 
 def main():
-	DWF_Push = "/fred/oz100/pipes/DWF_PIPE/CTIO_PUSH/"
-	parser = argparse.ArgumentParser(description='Handle File Ingests for the DWF pipeline', formatter_class=argparse.RawDescriptionHelpFormatter)
-	parser.add_argument('--push_dir',metavar='DIRECTORY',type=str,default=DWF_Push,
-	help='Directory where tarballs of compressed files are placed')
-	#parser.add_argument('-p', dest='processes', type=int, default=multiprocessing.cpu_count(),
-	#	help='Number of processes to plot with (default: #CPU cores)')
-	args = parser.parse_args()
-
-	path_to_watch = args.push_dir
-	path_to_untar = args.push_dir+'untar/'
-	path_to_sbatch = args.push_dir+'sbatch/'
-	before = dict ([(f, None) for f in os.listdir (path_to_watch)])
-
+    DWF_Push = "/fred/oz100/pipes/DWF_PIPE/CTIO_PUSH/"
+    parser = argparse.ArgumentParser(description='Handle File Ingests for the DWF pipeline', formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--push_dir',metavar='DIRECTORY',type=str,default=DWF_Push, help='Directory where tarballs of compressed files are placed')
+    parser.add_argument('--run_date', type=str, help='Date of the run night and data being unpacked')
+    #args = parser.parse_args()
+    args = parser.parse_args()
+    path_to_watch = args.push_dir
+    path_to_untar = args.push_dir+'untar/'
+    path_to_sbatch = args.push_dir+'sbatch/'
+    before = dict ([(f, None) for f in os.listdir (path_to_watch)])
+    run_date =args.run_date
 	#dwf_prepipe_unpack('DECam_00504110.tar',path_to_watch,path_to_untar,path_to_sbatch)
-	print('Monitoring Directory:'+path_to_watch)
-	while 1:
-		after = dict ([(f, None) for f in os.listdir (path_to_watch)])
-		added = [f for f in after if not f in before]
-		removed = [f for f in before if not f in after]
+    print('Monitoring Directory:'+path_to_watch)
+    while 1:
+        after = dict ([(f, None) for f in os.listdir (path_to_watch)])
+        added = [f for f in after if not f in before]
+        removed = [f for f in before if not f in after]
 
-		if added: print("Added: ", ", ".join (added))
-		if removed: print("Removed: ", ", ".join (removed))
+        if added: print("Added: ", ", ".join (added))
+        if removed: print("Removed: ", ", ".join (removed))
 
-		for f in added:
-			dwf_prepipe_unpack(f,path_to_watch,path_to_untar,path_to_sbatch)
+        for f in added:
+            dwf_prepipe_unpack(f,path_to_watch,path_to_untar,path_to_sbatch, run_date)
 
-		before = after
-		time.sleep (5)
+        before = after
+        time.sleep (5)
 
 
 if __name__ == '__main__':
