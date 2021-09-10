01bbcf00f3cbcf4b5d84348153bb22cebbd37744
Dougal Dobie <ddob1600@uni.sydney.edu.au>
2021-09-10 18:08:07 +1000

    initiate SBATCH vars

--- prepipe.py
+++ prepipe.py
@@ -18,11 +18,13 @@ res_name = 'dwf'
 ###############################################################
 
 class Prepipe:
-    def __init__(self, path_to_watch, path_to_untar, path_to_sbatch, run_date):
+    def __init__(self, path_to_watch, path_to_untar, path_to_sbatch, run_date, res_name):
         self.path_to_watch = path_to_watch
         self.path_to_untar = path_to_untar
         self.path_to_sbatch = path_to_sbatch
         self.run_date = run_date
+        
+        self.initiate_sbatch_vars(res_name)
     
     #Uncompress new file + create & submit assosciated sbatch scripts
     def unpack(self, file_name, ccdlist=None, n_per_ccd=15):
@@ -50,71 +52,90 @@ class Prepipe:
 	    for n in range(n_scripts):
 		    dwf_prepipe_sbatchccds(DECam_Root,DECam_Root+'_q'+str(n+1),ccdlist[n_per_ccd*n:(n+1)*n_per_ccd],sbatch_path,push_path, run_date)
 
-#Write Qsub Script & submit to queue
-def dwf_prepipe_sbatchccds(filename_root,qroot,ccds,sbatch_path,push_path, run_date):
-	image_list=[filename_root+'_'+f+'.jp2' for f in ccds]
+    
+    def initiate_sbatch_vars(self,
+                             res_name,
+                             walltime='00:05:00',
+                             queue='bryan',
+                             nodes='1',
+                             ppn='16',
+                             mem='90G',
+                             tmp='4G',
+                             ):
+        self.walltime = walltime
+        self.queue = queue
+        self.nodes = nodes
+        self.ppn = ppn
+        self.mem = mem
+        self.tmp = tmp
+        self.res_name = res_name
+        
+        
+    #Write Qsub Script & submit to queue
+    def dwf_prepipe_sbatchccds(filename_root,qroot,ccds,sbatch_path,push_path, run_date):
+	    image_list=[filename_root+'_'+f+'.jp2' for f in ccds]
 
-	sbatch_out_dir=sbatch_path+'out/'
+	    sbatch_out_dir=os.path.join(sbatch_path,'out/')
 
-	sbatch_name=sbatch_path+qroot+'.sbatch'
+	    sbatch_name=sbatch_path+qroot+'.sbatch'
 
-	print('Creating Script: '+sbatch_name+' for CCDs '+min(ccds)+' to '+max(ccds))
-	sbatch_file=open(sbatch_name,'w')
+	    print('Creating Script: {} for CCDs {} to {}'.format(sbatch_name, min(ccds), max(ccds))
+	    sbatch_file=open(sbatch_name,'w')
 
-	walltime='00:05:00'
-	queue='bryan'
-	nodes='1'
-	ppn='16'
-	mem='90G'
-	tmp='4G'
-	res=res_name
+	    walltime='00:05:00'
+	    queue='bryan'
+	    nodes='1'
+	    ppn='16'
+	    mem='90G'
+	    tmp='4G'
+	    res=res_name
 
-	sbatch_file.write('#!/bin/bash \n')
-	sbatch_file.write('#SBATCH -J {0}\n'.format(qroot))
-	sbatch_file.write('#SBATCH -o {0}{1}.stdout\n'.format(sbatch_path+'out/',qroot))
-	sbatch_file.write('#SBATCH -e {0}{1}.stderr\n'.format(sbatch_path+'out/',qroot))
-	sbatch_file.write('#SBATCH --time={0}\n'.format(walltime))
-	# sbatch_file.write('#SBATCH -p {0}\n'.format(queue))
-	sbatch_file.write('#SBATCH -A oz100\n')
-	sbatch_file.write('#SBATCH --nodes={0}\n'.format(nodes))
-	sbatch_file.write('#SBATCH --ntasks-per-node={0}\n'.format(ppn))
-	sbatch_file.write('#SBATCH --mem={0}\n'.format(mem))
-	sbatch_file.write('#SBATCH --tmp={0}\n'.format(tmp))
-	sbatch_file.write('#SBATCH --reservation={0}\n'.format(res))
+	    sbatch_file.write('#!/bin/bash \n')
+	    sbatch_file.write('#SBATCH -J {0}\n'.format(qroot))
+	    sbatch_file.write('#SBATCH -o {0}{1}.stdout\n'.format(sbatch_path+'out/',qroot))
+	    sbatch_file.write('#SBATCH -e {0}{1}.stderr\n'.format(sbatch_path+'out/',qroot))
+	    sbatch_file.write('#SBATCH --time={0}\n'.format(walltime))
+	    # sbatch_file.write('#SBATCH -p {0}\n'.format(queue))
+	    sbatch_file.write('#SBATCH -A oz100\n')
+	    sbatch_file.write('#SBATCH --nodes={0}\n'.format(nodes))
+	    sbatch_file.write('#SBATCH --ntasks-per-node={0}\n'.format(ppn))
+	    sbatch_file.write('#SBATCH --mem={0}\n'.format(mem))
+	    sbatch_file.write('#SBATCH --tmp={0}\n'.format(tmp))
+	    sbatch_file.write('#SBATCH --reservation={0}\n'.format(res))
 
-	sbatch_file.write('echo ------------------------------------------------------\n')
-	sbatch_file.write('echo Automated script by dwf_prepipe\n')
-	sbatch_file.write('echo ------------------------------------------------------\n')
-	sbatch_file.write('echo ------------------------------------------------------\n')
-	sbatch_file.write("echo -n 'Job is running on node '; cat $SLURM_NODELIST\n")
-	sbatch_file.write('echo ------------------------------------------------------\n')
-	sbatch_file.write('echo SBATCH: sbatch is running on $SLURM_SUBMIT_HOST\n')
-	sbatch_file.write('echo SBATCH: working directory is $SLURM_SUBMIT_DIR\n')
-	sbatch_file.write('echo SBATCH: job identifier is $SLURM_JOB_ID\n')
-	sbatch_file.write('echo SBATCH: job name is $SLURM_JOB_NAME\n')
+	    sbatch_file.write('echo ------------------------------------------------------\n')
+	    sbatch_file.write('echo Automated script by dwf_prepipe\n')
+	    sbatch_file.write('echo ------------------------------------------------------\n')
+	    sbatch_file.write('echo ------------------------------------------------------\n')
+	    sbatch_file.write("echo -n 'Job is running on node '; cat $SLURM_NODELIST\n")
+	    sbatch_file.write('echo ------------------------------------------------------\n')
+	    sbatch_file.write('echo SBATCH: sbatch is running on $SLURM_SUBMIT_HOST\n')
+	    sbatch_file.write('echo SBATCH: working directory is $SLURM_SUBMIT_DIR\n')
+	    sbatch_file.write('echo SBATCH: job identifier is $SLURM_JOB_ID\n')
+	    sbatch_file.write('echo SBATCH: job name is $SLURM_JOB_NAME\n')
 
-	sbatch_file.write('echo ------------------------------------------------------\n')
-	sbatch_file.write('echo Just in case, do the below:\n')
+	    sbatch_file.write('echo ------------------------------------------------------\n')
+	    sbatch_file.write('echo Just in case, do the below:\n')
 
 	
-	sbatch_file.write('echo $HOST\n')
+	    sbatch_file.write('echo $HOST\n')
 
-	for f in image_list:
-		#sbatch_file.write('/fred/oz100/dvohl/dwf_prepipe/dwf_prepipe_processccd.py -i {0} &\n'.format(f))
-		sbatch_file.write('~/dwf_prepipe/dwf_prepipe_processccd.py -i {0} -d {1} &\n'.format(f, run_date))
-	#desperate test
-	#sbatch_file.write('source ~/.bash_ozstarpython\n')
-	sbatch_file.write('wait\n')
+	    for f in image_list:
+		    #sbatch_file.write('/fred/oz100/dvohl/dwf_prepipe/dwf_prepipe_processccd.py -i {0} &\n'.format(f))
+		    sbatch_file.write('~/dwf_prepipe/dwf_prepipe_processccd.py -i {0} -d {1} &\n'.format(f, run_date))
+	    #desperate test
+	    #sbatch_file.write('source ~/.bash_ozstarpython\n')
+	    sbatch_file.write('wait\n')
 
-	sbatch_file.write('echo ------------------------------------------------------\n')
-	#sbatch_file.write('echo Safety Cleanup for the local disk:\n')
+	    sbatch_file.write('echo ------------------------------------------------------\n')
+	    #sbatch_file.write('echo Safety Cleanup for the local disk:\n')
 
-	#sbatch_file.write('rm $JOBFS/dwf/*.jp2\n')
-	#sbatch_file.write('rm $JOBFS/dwf/*.fits\n')
-	sbatch_file.write('echo ------------------------------------------------------\n')
+	    #sbatch_file.write('rm $JOBFS/dwf/*.jp2\n')
+	    #sbatch_file.write('rm $JOBFS/dwf/*.fits\n')
+	    sbatch_file.write('echo ------------------------------------------------------\n')
 
-	sbatch_file.close()
-	subprocess.run(['sbatch',sbatch_name])
+	    sbatch_file.close()
+	    subprocess.run(['sbatch',sbatch_name])
 
 def main():
     DWF_Push = "/fred/oz100/pipes/DWF_PIPE/CTIO_PUSH/"
