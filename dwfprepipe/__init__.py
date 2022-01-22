646f11514ea8f6475c050818c3fab1815525e64d
Dougal Dobie <ddob1600@uni.sydney.edu.au>
2022-01-22 13:36:19 +1100

    Add __init__ files

---
+++ dwfprepipe/__init__.py
@@ -0,0 +1,11 @@
+'''
+-------------------
+DWF Prepipe module.
+-------------------
+This module is a collection of useful scripts,
+functions and classes to be used to process DECam data
+as part of a DWF run.
+'''
+
+__author__ = 'Dougal Dobie'
+__version__ = "2.0.0-dev"
