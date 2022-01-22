fd477288db134c0b495f9ca20504a55581bbdea9
Dougal Dobie <ddob1600@uni.sydney.edu.au>
2022-01-22 16:57:40 +1100

    More fiddling

--- dwfprepipe/push.py
+++ dwfprepipe/push.py
@@ -12,6 +12,7 @@ import astropy.io.fits as pyfits
 
 import datetime
 import logging
+from pathlib import Path
 
 
 
@@ -79,36 +80,102 @@ def parse_args():
     return args
 
 class CTIOPush:
-    def __init__(self, path_to_watch, Qs, push_method, nbundle):
-        self.logger = logging.getLogger('dwf_prepipe.push.CTIOPush')
-        self.logger.debug('Created CTIOPush instance')
+    def __init__(self,
+                 path_to_watch: Union[str, Path],
+                 Qs: float,
+                 push_method: str,
+                 nbundle: int
+                 ):
+        """
+        Constructor method.
         
-        self.path_to_watch = path_to_watch
+        Args:
+            path_to_watch:
+            Qs:
+            push_method:
+            nbundle:
+        
+        Returns:
+            None
+        
+        Raises:
+            Exception: Invalid `push_method`
+        """
+        
+        valid_methods = {'s': 'serial',
+                         'p': 'parallel',
+                         'b': 'bundle',
+                         'l':'list',
+                         'e':'end of night',
+                         }
+        self.logger = logging.getLogger('dwf_prepipe.push.CTIOPush')
+        
+        if push_method in valid_methods.keys():
+            push_method = valid_methods[push_method]
+        
+        if push_method not in valid_methods.values():
+            raise Exception(
+                "The push method must be one of the following:"
+                "(s)erial, (p)arrallel, (b)undle, (l)ist, (e)nd of night."
+                "Please choose one of the above and try again."
+                )
+        
+        self.logger.info('Created CTIOPush instance')
+        
+        self.path_to_watch = Path(path_to_watch)
+        self.logger.info(f"Watching {path_to_watch}...")
+        
         self.Qs = Qs
+        self.logger.info(f"Compressing with Qs={Qs}...")
+        
         self.push_method = push_method
+        self.logger.info(f"Will transfer with {push_method} protocol...")
         self.nbundle = nbundle
         
-        self.jp2_dir=os.path.join(self.path_to_watch, 'jp2')
+        self.jp2_dir= self.path_to_watch / 'jp2'
         
-        self.set_g2_config()
+        self.set_ssh_config()
         
         
-    def set_g2_config(self,
-                      user='fstars',
-                      host='ozstar.swin.edu.au',
-                      push_dir=None,
-                      target_dir=None
+    def set_ssh_config(self,
+                      user: str = 'fstars',
+                      host: str = 'ozstar.swin.edu.au',
+                      push_dir: Union[str, None] = None,
+                      target_dir: Union[str, None] = None
                       ):
+        """
+        Set the ssh variables and target/destination directories
+        
+        Args:
+            user: account username
+            host: destination host
+            push_dir: directory to push
+            target_dir:
+        
+        Returns:
+            None
+        """
         
         self.user = user
         self.host = host
-        self.reciever = '{}@{}'.format(user, host)
-        self.push_dir=push_dir
-        self.target_dir=target_dir
+        self.reciever = f'{user}@{host}'
+        self.push_dir = push_dir
+        self.target_dir = target_dir
         
-    def dwf_prepipe_validatefits(self, file_name):
+    def dwf_prepipe_validatefits(self, file_name: Union[str, Path]):
+        """
+        Validate the fits file
+        
+        Args:
+            file_name: path to the fits file
+        
+        Returns:
+            None
+        """
         warnings.filterwarnings('error','.*File may have been truncated:.*',UserWarning)
-        valid=0
+        
+        valid=False
+        
         while(not valid):
             try:
                 test=pyfits.open(file_name)
@@ -123,44 +190,56 @@ class CTIOPush:
                 time.sleep(0.5)
             else:
                 self.logger.info('{} passes validation'.format(file_name))
-                valid=1
+                valid=True
 
     #Package new raw .fits.fz file
-    def packagefile(self, file_name):
-        file_name=file_name.split('/')[-1].split('.')[0]
+    def packagefile(self, filepath: Union[str, Path]):
+        """
+        Package a file ready for shipping
+        
+        Args:
+            filepath: path to the file
+            
+        Returns
+            None
+        """
+        
+        filepath = Path(filepath)
+        file_name = filepath.name
         
         self.logger.info('Unpacking: {}'.format(file_name))
         
-        fz_path = os.path.join(data_dir, '{}.fits.fz'.format(file_name))
-        subprocess.run(['funpack', fz_path])
+        fz_path = self.data_dir / file_name.with_suffix('.fits.fz')
         
-        jp2_dest = self.jp2_dir+file_name
+        subprocess.run(['funpack', str(fz_path)])
+        
+        jp2_dest = self.jp2_dir / file_name
         if not os.path.isdir(os.path.join(jp2_dest)):
             self.logger.info('Creating Directory: {}'.format(jp2_dest))
             os.makedirs(p2_path)
         
         self.logger.info('Compressing: {}'.format(file_name))
-        fitsfile = file_name+'.fits'
-        jp2file = file_name+'.jp2'
+        fitsfile = file_name.with_suffix('.fits')
+        jp2file = file_name.with_suffix('.jp2')
         
         subprocess.run(['time',
                         'f2j_DECam',
                         '-i',
-                        os.path.join(self.data_dir, fitsfile),
+                        str(self.data_dir / fitsfile),
                         '-o',
-                        os.path.join(jp2_path, jp2file),
-                        'Qstep={}'.format(self.Qs)),
+                        str(self.jp2_dir / jp2file),
+                        f'Qstep={self.Qs}',
                         '-num_threads',
                         '1']
                         )
         
-        packaged_file = self.jp2_dir+file_name+'.tar'
+        packaged_file = self.jp2_dir / file_name.with_suffix('.tar')
         self.logger.info('Packaging: {}'.format(packaged_file))
         subprocess.run(['tar',
                         '-cf',
-                        packaged_file,
+                        str(packaged_file),
                         '-C',
-                        jp2_path,
+                        str(jp2_path),
                         '.']
                         )
 
