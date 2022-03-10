e3180f13aed86d6a7da18e82ae4b309b90c36d22
Dougal Dobie <ddobie@swin.edu.au>
2022-03-10 18:40:51 +1100

    Removed rubbish

--- bin/loop_untared.py
+++
@@ -1,8 +0,0 @@
-import numpy as np 
-import os 
-
-file_directory = '/fred/oz100/pipes/DWF_PIPE/CTIO_PUSH/untar/'
-for filename in os.listdir(file_directory):
-    if filename.startswith('DECam_009123'):
-        os.system('python dwf_prepipe_processccd.py -i ' + filename)
-
