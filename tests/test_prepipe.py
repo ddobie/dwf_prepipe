e8b61300f97acfc5002952ad99fba16a7a06359c
Dougal Dobie <ddobie@swin.edu.au>
2022-03-14 18:31:51 +1100

    Added empty test files

---
+++ tests/test_prepipe.py
@@ -0,0 +1,7 @@
+import pytest
+import dwfprepipe.prepipe as prepipe
+
+class TestPrepipe:
+    """
+    This class contains all the tests related to the CTIOPush object.
+    """
---
+++ tests/test_push.py
@@ -0,0 +1,7 @@
+import pytest
+import dwfprepipe.push as push
+
+class TestCTIOPush:
+    """
+    This class contains all the tests related to the CTIOPush object.
+    """
